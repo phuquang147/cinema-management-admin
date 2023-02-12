5bf53b009491e902eb682caae643d5a4ffc5ad45
Phu Quang <86101765+phuquang147@users.noreply.github.com>
2023-02-12 21:46:01 +0700

    Login page

---
+++ src/components/Login/LoginForm.tsx
@@ -0,0 +1,122 @@
+import { yupResolver } from "@hookform/resolvers/yup";
+import { LoadingButton } from "@mui/lab";
+import { IconButton, InputAdornment, Stack, Typography } from "@mui/material";
+import Cookies from "js-cookie";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { useDispatch } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import * as Yup from "yup";
+import FormProvider from "~/components/Form/FormProvider";
+import RHFTextField from "~/components/Form/RHFTextField";
+import Iconify from "~/components/Iconify";
+// import { setDataStarted } from "~/redux/dataSlice";
+// import { login } from "~/services/authServices";
+
+export default function LoginForm() {
+  // const navigate = useNavigate();
+  const [showPassword, setShowPassword] = useState(false);
+
+  const LoginSchema = Yup.object().shape({
+    username: Yup.string().required("Vui lòng nhập tên đăng nhập"),
+    password: Yup.string().required("Vui lòng nhập mật khẩu"),
+  });
+
+  const defaultValues = {
+    username: "",
+    password: "",
+  };
+
+  const methods = useForm({
+    resolver: yupResolver(LoginSchema),
+    defaultValues,
+  });
+
+  const {
+    handleSubmit,
+    formState: { isSubmitting },
+    getValues,
+  } = methods;
+
+  const dispatch = useDispatch();
+
+  const onSubmit = async () => {
+    // const { username, password } = getValues();
+    // try {
+    //   const { data, status } = await login({ username, password });
+    //   if (status === 200) {
+    //     const { token, accountId } = data;
+    //     const remainingMilliseconds = 60 * 60 * 1000;
+    //     const expiryDate = new Date(
+    //       new Date().getTime() + remainingMilliseconds
+    //     );
+    //     Cookies.set("token", token, { expires: expiryDate });
+    //     Cookies.set("accountId", accountId, { expires: expiryDate });
+    //     toast.success("Đăng nhập thành công");
+    //     navigate("/", { replace: true });
+    //     dispatch(setDataStarted());
+    //   } else toast.error("Đã có lỗi xảy ra! Vui lòng thử lại");
+    // } catch (err) {
+    //   toast.error(err.response.data.message);
+    // }
+  };
+
+  return (
+    <FormProvider methods={methods} onSubmit={handleSubmit(onSubmit)}>
+      <Stack spacing={3}>
+        <RHFTextField name="username" label="Tên đăng nhập" />
+
+        <RHFTextField
+          name="password"
+          label="Mật khẩu"
+          type={showPassword ? "text" : "password"}
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end">
+                <IconButton
+                  onClick={() => setShowPassword(!showPassword)}
+                  edge="end"
+                >
+                  <Iconify
+                    icon={showPassword ? "eva:eye-fill" : "eva:eye-off-fill"}
+                  />
+                </IconButton>
+              </InputAdornment>
+            ),
+          }}
+        />
+      </Stack>
+
+      <Stack
+        direction="row"
+        alignItems="center"
+        justifyContent="end"
+        sx={{ my: 2, textDecoration: "none" }}
+      >
+        <Typography
+          component={Link}
+          to="/forgot-password"
+          color="primary"
+          sx={{
+            textDecoration: "none !important",
+            fontWeight: 700,
+            fontSize: "14px",
+          }}
+        >
+          Quên mật khẩu?
+        </Typography>
+      </Stack>
+
+      <LoadingButton
+        fullWidth
+        size="large"
+        type="submit"
+        variant="contained"
+        loading={isSubmitting}
+      >
+        Đăng Nhập
+      </LoadingButton>
+    </FormProvider>
+  );
+}
