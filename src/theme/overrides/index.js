a274f75bc90e1c3bcdef0a321b8905c6cb6e774d
Phu Quang <86101765+phuquang147@users.noreply.github.com>
2023-02-12 13:41:11 +0700

    Override theme

---
+++ src/theme/overrides/index.js
@@ -0,0 +1,23 @@
+import Autocomplete from "./Autocomplete";
+import Backdrop from "./Backdrop";
+import Button from "./Button";
+import Card from "./Card";
+import CssBaseline from "./CssBaseline";
+import Input from "./Input";
+import Paper from "./Paper";
+import Tooltip from "./Tooltip";
+import Typography from "./Typography";
+
+export default function ComponentsOverrides(theme) {
+  return Object.assign(
+    Card(theme),
+    Input(theme),
+    Paper(theme),
+    Button(theme),
+    Tooltip(theme),
+    Backdrop(theme),
+    Typography(theme),
+    CssBaseline(theme),
+    Autocomplete(theme)
+  );
+}
