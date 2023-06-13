8aaca5fbc92a78e355609696b4f119742851f0b4
Phu Quang <86101765+phuquang147@users.noreply.github.com>
2023-06-13 23:34:25 +0700

    Add dayjs plugins

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,6 @@
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
+import updateLocale from "dayjs/plugin/updateLocale";
 import Cookie from "js-cookie";
 import React, { useEffect } from "react";
 import { ToastContainer } from "react-toastify";
@@ -14,6 +17,27 @@ const App: React.FC = () => {
       dispatch(getUser(JSON.parse(user)));
     }
   }, [dispatch]);
+
+  dayjs.extend(updateLocale);
+  dayjs.extend(relativeTime);
+
+  dayjs.updateLocale("en", {
+    relativeTime: {
+      future: "%s",
+      past: "% ngày trước",
+      s: "vài giây trước",
+      m: "một phút trước",
+      mm: "%d phút trước",
+      h: "1 giờ trước",
+      hh: "%d giờ trước",
+      d: "1 ngày trước",
+      dd: "%d ngày trước",
+      M: "1 tháng trước",
+      MM: "%d tháng trước",
+      y: "1 năm trước",
+      yy: "%d năm trước",
+    },
+  });
 
   return (
     <>
